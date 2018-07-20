ef7cf237ff751d5cf0e07a193515adce658bbcf4
danrodz <daniel.a.rodriguez@dr.com>
2018-07-19 19:02:08 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    return true if self[-1] == "."
+    false
   end
 
   def question?
-
+    return true if self[-1] == "?"
+    false
   end
 
   def exclamation?
-
+    return true if self[-1] == "!"
+    false
   end
 
   def count_sentences
-
+    self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
   end
 end
